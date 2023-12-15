1ed0af9cbb78dd9515949d0a2ddff768aa6c7310
Aeon48 <abhishektuladhar78@gmail.com>
2023-12-15 10:28:57 +0545

    SignUpPage

--- Nepfilili/Nepfilili/src/Pages/SignUpPage.jsx
+++ Nepfilili/Nepfilili/src/Pages/SignUpPage.jsx
@@ -2,26 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function SignUpPage() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    async function handleSubmit(e){
-        e.preventDefault();
-
-        try{
-                await axios.post("http://localhost:8000/signup", {
-                    email,password
-                })
-
-        }
-        catch(e){
-            console.log(e);
-        
-    }
-
   return (
-
     <div>
         <section className='bg-gray-50 min-h-screen flex items-center justify-center'>
             
@@ -30,23 +11,23 @@ function SignUpPage() {
                 <div className='w-1/2 px-10'>
                     <h2 className='font-bold flex justify-center text-2xl'>Sign Up</h2>
 
-                    <form onSubmit={handleSubmit} action="POST" className='flex flex-col gap-5'>
-                        <input id='' type="text" name='name' placeholder='Name' className='p-2 mt-10 rounded-xl border' onChange={(e)=> setName(e.target.value)}/>
+                    <form action="POST" className='flex flex-col gap-5'>
+                        <input id='' type="text" name='name' placeholder='Name' className='p-2 mt-10 rounded-xl border'/>
 
-                        <input id='' type="text" name='username' placeholder='Username' className='p-2 rounded-xl border' onChange={(e)=> setName(e.target.value)}/>
+                        <input id='' type="text" name='username' placeholder='Username' className='p-2 rounded-xl border'/>
 
-                        <input id='' type="text" name='username' placeholder='Address' className='p-2 rounded-xl border' onChange={(e)=> setName(e.target.value)}/>
+                        <input id='' type="text" name='username' placeholder='Address' className='p-2 rounded-xl border'/>
 
-                        <input id='' type="password" className='p-2 rounded-xl border' name='password' placeholder='Password' onChange={(e)=> setPassword(e.target.value)}/>
+                        <input id='' type="password" className='p-2 rounded-xl border' name='password' placeholder='Password'/>
                         
-                        <input id='' type="tel" name='number' placeholder='Phone Number' className='p-2 rounded-xl border' onChange={(e)=> setName(e.target.value)}/>
+                        <input id='' type="tel" name='number' placeholder='Phone Number' className='p-2 rounded-xl border'/>
 
-                        <input id='' type="text" className='p-2 rounded-xl border' name='email' placeholder='Email' onChange={(e)=>{setEmail(e.target.value)}} />
+                        <input id='' type="text" className='p-2 rounded-xl border' name='email' placeholder='Email'/>
 
-                        <input id='' type="password" className='p-2 rounded-xl border' name='password' placeholder='Password' onChange={(e)=> setPassword(e.target.value)}/>
+                        <input id='' type="password" className='p-2 rounded-xl border' name='password' placeholder='Password'/>
                         
 
-                        <button className='bg-[#6d757a] rounded-full text-white py-2 mt-3 hover:scale-105 duration-300'>Create Account</button>
+                        <button type='submit' className='bg-[#6d757a] rounded-full text-white py-2 mt-3 hover:scale-105 duration-300'>Create Account</button>
                     </form>
 
                     <div className='text-sm flex justify-center mt-6'>
