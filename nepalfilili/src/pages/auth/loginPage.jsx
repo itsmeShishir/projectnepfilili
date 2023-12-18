5c2884c7862adf5f60ecda03026d39c96bb97b6a
haula <motivate23a@gmail.com>
2023-12-18 12:42:32 +0545

    Merge auth branch into main

--- nepalfilili/src/pages/auth/loginPage.jsx
+++ nepalfilili/src/pages/auth/loginPage.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React, {useState} from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/images/sidepic.jpg' 
-import googlelogo from '../../assets/images/googleicon.svg'
-
-function LoginPage() {
-  const [data, setData] = useState({username: "", password: ""});
-
-  const handleChange=(e)=>{
-    setData((prevData)=>({
-      ...prevData,
-      [e.target.name]: e.target.value
-=======
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -26,38 +11,11 @@ function LoginPage() {
     setData((prevData) => ({
       ...prevData,
       [e.target.name]: e.target.value,
->>>>>>> auth
     }));
   };
 
   return (
     <div>
-<<<<<<< HEAD
-      <div className='container d-flex justify-content-center align-items-center min-vh-100'>
-
-
-        <div className='row border p-2 bg-white shadow box-area w-50'>
-              <img src={logo} className="img-fluid mt-2 mb-2" style={{width: "300px"}} />
-
-          <form action='POST' className='col-md-6 right-box'>
-            <div className='row align-items-center'>
-              <div className="header-text mb-4 mx-2">
-                <h1 className='text-center fs-2 mt-4'>Sign In</h1>
-              </div>
-
-              <div className='input-group mb-3'> 
-                <input type="email" className='form-control form-control-lg bg-light fs-6' placeholder='Enter your Email' onChange={handleChange} value={data.username} required />
-              </div>
-
-              <div className='input-group mb-1'> 
-                <input type="password" className='form-control form-control-lg bg-light fs-6' placeholder='Enter your Password' onChange={handleChange} value={data.password} required />
-              </div>
-
-              <div className='input-group mt-2 d-flex justify-content-between'>
-                <div className='form-check'>
-                  <input type="checkbox" className='form-check-input' id='formCheck' />
-                  <label for="formCheck" className='form-check-label text-secondary'>
-=======
       <div className="container d-flex justify-content-center align-items-center min-vh-100">
         <div className="row border p-2 bg-white shadow box-area w-50">
           <img
@@ -105,36 +63,17 @@ function LoginPage() {
                     for="formCheck"
                     className="form-check-label text-secondary"
                   >
->>>>>>> auth
                     <small>Remember Me</small>
                   </label>
                 </div>
 
-<<<<<<< HEAD
-                <div className='forgot'>
-=======
                 <div className="forgot">
->>>>>>> auth
                   <small>
                     <a href="#">Forgot Password?</a>
                   </small>
                 </div>
               </div>
 
-<<<<<<< HEAD
-              <div className='input-group mt-5'>
-                <button className='btn btn-lg btn-primary w-100 fs-6'>Login</button>
-              </div>
-
-              <div className='input-group mt-3 mb-2'>
-                <button className='btn btn-lg btn-light w-100 fs-6'><img src={googlelogo} style={{width: "30px"}} alt=""  className='me-2'/>Sign in with Google</button>
-              </div>
-              <div className='row mb-2 fs-6'>
-                <small>Don't have an account? <Link to="/auth/register" className='mx-2 justify-content-center'>Sign-Up</Link></small>
-              </div>
-            </div>
-
-=======
               <div className="input-group mt-5">
                 <button className="btn btn-lg btn-primary w-100 fs-6">
                   Login
@@ -164,16 +103,11 @@ function LoginPage() {
                 </small>
               </div>
             </div>
->>>>>>> auth
           </form>
         </div>
       </div>
     </div>
-<<<<<<< HEAD
-  )
-=======
   );
->>>>>>> auth
 }
 
-export default LoginPage
+export default LoginPage;
--- nepalfilili/src/pages/auth/registerPage.jsx
+++ nepalfilili/src/pages/auth/registerPage.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/images/sidepic.jpg' 
-import googlelogo from '../../assets/images/googleicon.svg'
-
-function LoginPage() {
-  const [data, setData] = useState({name: "", username: "",email: "",address: "",phone: "",password: "",});
-
-  const handleChange=(e)=>{
-    setData((prevData)=>({
-=======
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/sidepic.jpg";
@@ -27,61 +15,13 @@ function LoginPage() {
 
   const handleChange = (e) => {
     setData((prevData) => ({
->>>>>>> auth
       ...prevData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     }));
   };
 
   return (
     <div>
-<<<<<<< HEAD
-      <div className='container d-flex justify-content-center align-items-center min-vh-100'>
-
-
-        <div className='row border p-2 bg-white shadow box-area w-50'>
-              <img src={logo} className="img-fluid mt-2 mb-2" style={{width: "300px"}} />
-
-          <form action='POST' className='col-md-6 right-box'>
-            <div className='row align-items-center'>
-              <div className="header-text mb-3 mx-2">
-                <h1 className='font-weight-bold text-center fs-2 mt-4 mb-3'>Sign Up</h1>
-              </div>
-
-              <div className='input-group mb-2'>
-                  <input type="text" id='name' name='name' value={data.name} placeholder='Name' className='form-control form-control-lg bg-light fs-6'  /> 
-               </div>
-
-               <div className='input-group mb-2'>
-                  <input type="text" id='username' name='username' value={data.username} placeholder='Username' className='form-control form-control-lg bg-light fs-6'  /> 
-               </div>
-
-               <div className='input-group mb-2'>
-                  <input type="text" id='address' name='address' value={data.address} placeholder='Address' className='form-control form-control-lg bg-light fs-6'  /> 
-               </div>
-
-               <div className='input-group mb-2'>
-                  <input type="tel" id='phone' name='phone' value={data.phone} placeholder='Phone Number' className='form-control form-control-lg bg-light fs-6'  /> 
-               </div>
-
-               <div className='input-group mb-2'>
-                  <input type="email" id='email' name='email' value={data.email} placeholder='Email' className='form-control form-control-lg bg-light fs-6'  /> 
-               </div>
-
-               <div className='input-group mb-2'>
-                  <input type="password" id='password' name='password' value={data.password} placeholder='Password' className='form-control form-control-lg bg-light fs-6'  /> 
-               </div>
-
-              <div className='input-group mt-4'>
-                <button className='btn btn-lg btn-primary w-100 fs-6'>Sign Up</button>
-              </div>
-
-              <div className='input-group mt-3 mb-2'>
-                <button className='btn btn-lg btn-light w-100 fs-6'><img src={googlelogo} style={{width: "30px"}} alt=""  className='me-2'/>Sign Up with Google</button>
-              </div>
-            </div>
-
-=======
       <div className="container d-flex justify-content-center align-items-center min-vh-100">
         <div className="row border p-2 bg-white shadow box-area w-50">
           <img
@@ -182,19 +122,11 @@ function LoginPage() {
                 </button>
               </div>
             </div>
->>>>>>> auth
           </form>
         </div>
       </div>
     </div>
-<<<<<<< HEAD
-  )
-}
-
-export default LoginPage
-=======
   );
 }
 
 export default LoginPage;
->>>>>>> auth
