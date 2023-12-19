3e4540a5eb3bffcaebdff1261195f273e85addb2
haula <motivate23a@gmail.com>
2023-12-19 12:12:25 +0545

    Home Section Completed in Home Page

--- nepalfilili/src/pages/frontend/homePage.js
+++ nepalfilili/src/pages/frontend/homePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HeaderComponent from "../../components/header/headerComponent";
 import FooterComponent from "../../components/footer/footerComponent";
+import MainSectionComponent from "../../components/home/MainSectionComponent";
 const HomePage = () => {
   return (
     <>
       <HeaderComponent />
-      <p>We Provide best It Support in the industry</p>
+      <MainSectionComponent />
       <FooterComponent />
     </>
   );
