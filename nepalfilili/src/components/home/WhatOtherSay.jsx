74b3bde9c33f1dbc991383043073f0c7c7246a29
haula <motivate23a@gmail.com>
2023-12-21 11:14:51 +0545

    What Our Customer Say has been completed

--- nepalfilili/src/components/home/WhatOtherSay.jsx
+++ nepalfilili/src/components/home/WhatOtherSay.jsx
@@ -1,191 +1,198 @@
 import React from "react";
+import { FaComments } from "react-icons/fa6";
 
 const WhatOtherSay = () => {
   return (
     <div>
-      <div className="container mt-5 mb-5">
-        <div className="row g-2">
-          <div className="col-md-4">
-            <div className="card p-3 text-center px-4">
-              <div className="user-image">
-                <img
-                  src="https://i.imgur.com/PKHvlRS.jpg"
-                  className="rounded-circle"
-                  width="80"
-                />
-              </div>
+      <div className="container text-center">
+        <FaComments className="fs-1 mt-5 " />
+        <h3 className="about fs-1 text-center text-primary">
+          What do Company Owners Say?
+        </h3>
+        <div className="container mt-5 mb-5">
+          <div className="row g-2">
+            <div className="col-md-4">
+              <div className="card p-3 text-center px-4">
+                <div className="user-image">
+                  <img
+                    src="https://i.imgur.com/PKHvlRS.jpg"
+                    className="rounded-circle"
+                    width="80"
+                  />
+                </div>
 
-              <div className="user-content">
-                <h5 className="mb-0">Bruce Hardy</h5>
-                <span>Software Developer</span>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
+                <div className="user-content">
+                  <h5 className="mb-1 text-primary">Bruce Hardy</h5>
+                  <span className="text-danger">Software Developer</span>
+                  <p className="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
 
-              <div className="ratings">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card p-3 text-center px-4">
-              <div className="user-image">
-                <img
-                  src="https://i.imgur.com/w2CKRB9.jpg"
-                  className="rounded-circle"
-                  width="80"
-                />
-              </div>
-
-              <div className="user-content">
-                <h5 className="mb-0">Mark Smith</h5>
-                <span>Web Developer</span>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
-
-              <div className="ratings">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
+                <div className="ratings">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-md-4">
-            <div className="card p-3 text-center px-4">
-              <div className="user-image">
-                <img
-                  src="https://i.imgur.com/ACeArwY.jpg"
-                  className="rounded-circle"
-                  width="80"
-                />
+            <div className="col-md-4">
+              <div className="card p-3 text-center px-4">
+                <div className="user-image">
+                  <img
+                    src="https://i.imgur.com/w2CKRB9.jpg"
+                    className="rounded-circle"
+                    width="80"
+                  />
+                </div>
+
+                <div className="user-content">
+                  <h5 className="mb-0">Mark Smith</h5>
+                  <span>Web Developer</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
+
+                <div className="ratings">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
               </div>
+            </div>
 
-              <div className="user-content">
-                <h5 className="mb-0">Veera Duncan</h5>
-                <span>Software Architect</span>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
+            <div className="col-md-4">
+              <div className="card p-3 text-center px-4">
+                <div className="user-image">
+                  <img
+                    src="https://i.imgur.com/ACeArwY.jpg"
+                    className="rounded-circle"
+                    width="80"
+                  />
+                </div>
 
-              <div className="ratings">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
+                <div className="user-content">
+                  <h5 className="mb-0">Veera Duncan</h5>
+                  <span>Software Architect</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
+
+                <div className="ratings">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="container mt-5 mb-5">
-        <div className="row g-2">
-          <div className="col-md-4">
-            <div className="card p-3 text-center px-4">
-              <div className="user-image">
-                <img
-                  src="https://i.imgur.com/PKHvlRS.jpg"
-                  className="rounded-circle"
-                  width="80"
-                />
-              </div>
+        <div className="container mt-5 mb-5">
+          <div className="row g-2">
+            <div className="col-md-4">
+              <div className="card p-3 text-center px-4">
+                <div className="user-image">
+                  <img
+                    src="https://i.imgur.com/PKHvlRS.jpg"
+                    className="rounded-circle"
+                    width="80"
+                  />
+                </div>
 
-              <div className="user-content">
-                <h5 className="mb-0">Bruce Hardy</h5>
-                <span>Software Developer</span>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
+                <div className="user-content">
+                  <h5 className="mb-0">Bruce Hardy</h5>
+                  <span>Software Developer</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
 
-              <div className="ratings">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card p-3 text-center px-4">
-              <div className="user-image">
-                <img
-                  src="https://i.imgur.com/w2CKRB9.jpg"
-                  className="rounded-circle"
-                  width="80"
-                />
-              </div>
-
-              <div className="user-content">
-                <h5 className="mb-0">Mark Smith</h5>
-                <span>Web Developer</span>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
-
-              <div className="ratings">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
+                <div className="ratings">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-md-4">
-            <div className="card p-3 text-center px-4">
-              <div className="user-image">
-                <img
-                  src="https://i.imgur.com/ACeArwY.jpg"
-                  className="rounded-circle"
-                  width="80"
-                />
+            <div className="col-md-4">
+              <div className="card p-3 text-center px-4">
+                <div className="user-image">
+                  <img
+                    src="https://i.imgur.com/w2CKRB9.jpg"
+                    className="rounded-circle"
+                    width="80"
+                  />
+                </div>
+
+                <div className="user-content">
+                  <h5 className="mb-0">Mark Smith</h5>
+                  <span>Web Developer</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
+
+                <div className="ratings">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
               </div>
+            </div>
 
-              <div className="user-content">
-                <h5 className="mb-0">Veera Duncan</h5>
-                <span>Software Architect</span>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-              </div>
+            <div className="col-md-4">
+              <div className="card p-3 text-center px-4">
+                <div className="user-image">
+                  <img
+                    src="https://i.imgur.com/ACeArwY.jpg"
+                    className="rounded-circle"
+                    width="80"
+                  />
+                </div>
 
-              <div className="ratings">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
+                <div className="user-content">
+                  <h5 className="mb-0">Veera Duncan</h5>
+                  <span>Software Architect</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </div>
+
+                <div className="ratings">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
               </div>
             </div>
           </div>
