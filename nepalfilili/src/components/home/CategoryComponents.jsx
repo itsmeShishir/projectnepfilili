d4f48d1d399c7725787107572b60cccd74e0bbbf
Aeon48 <abhishektuladhar78@gmail.com>
2023-12-26 09:51:14 +0545

    Jobs Page

--- nepalfilili/src/components/home/CategoryComponents.jsx
+++ nepalfilili/src/components/home/CategoryComponents.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BiSolidDirections } from "react-icons/bi";
-import LocationComponents from "../header/locationComponent";
+import LocationComponents from "../propss/locationComponent";
 
 const CategoryComponents = () => {
   return (
