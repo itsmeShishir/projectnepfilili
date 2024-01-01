4550bbc8f256336e94490f25582797bdd21fba9f
Aeon48 <abhishektuladhar78@gmail.com>
2024-01-01 11:57:11 +0545

    commit

--- frontend/src/pages/auth/Login/login.jsx
+++ frontend/src/pages/auth/Login/login.jsx
@@ -1,10 +1,55 @@
 import React from "react";
 import Header from "../../../components/LandingPage/Layout/Header/Header";
 import Footer from "../../../components/LandingPage/Layout/Footer/Footer";
+import { FaUser } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import "./login.css"
+
 const Login = () => {
   return (
     <div>
       <Header />
+
+      <div>
+       <div className="sign-in-container">
+        <div className="sign-in-box">
+          <div className="sign-in-header">
+            <h2>Login</h2>
+          </div>
+
+          <div className="input-box">
+            <input type="text" id='user' placeholder='Email' className='input-field' required />
+
+            <FaUser className='Icon' />
+          </div>
+
+          <div className="input-box">
+            <input type="password" id='pass' className='input-field' placeholder='Password' required />
+            <FaEyeSlash className='Icon' />
+          </div>
+
+          <div className="remember-forgot">
+            <div className="remember-me">
+              <input type="checkbox" id='remember' />
+              <label htmlFor="remember">Remember me</label>
+            </div>
+
+            <div className="forgot">
+              <a href="#">Forgot password?</a>
+            </div>
+          </div>
+
+          <div className="input_box">
+              <input type="submit" className='input-submit' value="Login"  />
+            </div>
+
+            <div className="register">
+              <span>Don't have an account? <a href="#">Register</a></span>
+            </div>
+        </div>
+       </div>
+    </div>
+
       <Footer />
     </div>
   );
--- frontend/src/pages/auth/Register/register.jsx
+++ frontend/src/pages/auth/Register/register.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import "./signup.css"
+
 
 const Register = () => {
-  return <div>Register</div>;
+  return <div>
+
+<div>
+    <div className="sign-up-container">
+     <div className="sign-up-box">
+       <div className="sign-up-header">
+         <h2>Sign Up</h2>
+       </div>
+
+       <div className="input-box-first-name">
+         <input type="text" id='user' placeholder='First Name' className='input-field' required />
+       </div>
+
+       <div className="input-box-last-name">
+         <input type="text" id='user' placeholder='Last Name' className='input-field' required />
+       </div>
+
+       <div className="input-box-address">
+         <input type="text" id='user' placeholder='Address' className='input-field' required />
+       </div>
+       
+       <div className="input-box-phone">
+         <input type="tel" id='user' placeholder='Phone Number' className='input-field' required />
+       </div>
+
+       <div className="input-box">
+         <input type="email" id='user' placeholder='Email' className='input-field' required />
+       </div>
+
+       <div className="input-box">
+         <input type="password" id='pass' className='input-field' placeholder='Password' required />
+       </div>
+
+       <div className="remember-forgot">
+         <div className="remember-me">
+           <input type="checkbox" id='remember' />
+           <label htmlFor="remember">Remember me</label>
+         </div>
+
+         <div className="forgot">
+           <a href="#">Forgot password?</a>
+         </div>
+       </div>
+
+       <div className="input_box">
+           <input type="submit" className='input-submit' value="Sign-Up"  />
+         </div>
+
+         <div className="register">
+           <span>Don't have an account? <a href="#">Register</a></span>
+         </div>
+     </div>
+    </div>
+ </div>    
+
+  </div>;
 };
 
 export default Register;
